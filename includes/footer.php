6c1a880329947ca9675b9598d79925c8d808ce38
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-11-01 09:23:54 -0700

    Added footer.php

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer>
+        <div class = "copyright">
+            <div class="row">
+            <ul>
+                <li>Copyright &copy; <?php echo date('Y');?></li>
+                <li>All Rights Reserved.</li>
+                <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fkatebaldwin.dev%2FIT261%2Findex.php" target="_blank">Valid HTML</a></li>
+                <li><a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a></li>
+            </ul>
+            </div> <!-- end row -->
+        </div> <!-- end copyright -->
+    </footer>
+</body>
+</html>
