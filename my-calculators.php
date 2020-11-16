1b234ed3e8b34ee647e2ec292b9d1d331a1d6311
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-11-16 14:40:16 -0700

    Added my-calculators.php

---
+++ my-calculators.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<meta charset="UTF-8"> 
+<title>My Calculators</title>
+</head>
+<body>
+    <ul>
+        <li><a href="calculator.php">calculator.php</a></li>
+        <li><a href="calculator-days.php">calculator-days.php</a></li>
+        <li><a href="calculator-days-errors.php">calculator-days-errors.php</a></li>
+        <li><a href="calculator-days-errors-sticky.php">calculator-days-errors-sticky.php</a></li>
+    </ul>
+</body>
+</html>
