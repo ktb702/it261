a2277b11194cb416123d2b14222242818d8b7650
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-11-20 07:43:30 -0700

    Updated footer with new HTML Validation links

--- website/includes/footer.php
+++ website/includes/footer.php
@@ -11,7 +11,10 @@
      ?> </li>
     <li>All Rights Reserved</li>
     <li>Web Design by Kate</li>
-    <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fkatebaldwin.dev%2FIT261%2Fwebsite%2Findex.php">Valid HTML</a></li>
+    <li>   <a href="https://validator.w3.org/check?uri=referer" 
+     onclick="this.href='https://validator.w3.org/check?uri=' + document.URL">New HTML Validation</a></li>
+
+    <!-- <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fkatebaldwin.dev%2FIT261%2Fwebsite%2Findex.php">Valid HTML</a></li> -->
     <li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fkatebaldwin.dev%2FIT261%2Fwebsite%2Findex.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">Valid CSS</a></li>
     </ul> 
 </footer>
