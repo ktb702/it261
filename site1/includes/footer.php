279d513581d4cf4ed16830aacfd1f61791a86300
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-11-20 07:51:14 -0700

    Updated footer with new HTML Validation link

--- site1/includes/footer.php
+++ site1/includes/footer.php
@@ -4,7 +4,9 @@
             <ul>
                 <li>Copyright &copy; <?php echo date('Y');?></li>
                 <li>All Rights Reserved.</li>
-                <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fkatebaldwin.dev%2FIT261%2Fsite1%2Fdaily.php" target="_blank">Valid HTML</a></li>
+                <li>   <a href="https://validator.w3.org/check?uri=referer" 
+                onclick="this.href='https://validator.w3.org/check?uri=' + document.URL">New HTML Validation</a></li>
+                <!-- <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fkatebaldwin.dev%2FIT261%2Fsite1%2Fdaily.php" target="_blank">Valid HTML</a></li> -->
                 <li><a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a></li>
             </ul>
             </div> <!-- end row -->
