070b9bf1713814db807e0b79bed3decf5b3d3bd1
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-11-29 21:32:43 -0800

    Updated gallery.php

--- website/gallery.php
+++ website/gallery.php
@@ -18,7 +18,7 @@ include('includes/header.php');
     </table>
 </main>
 <aside>
-    <h3>This is my headline 3 on the gallery page</h3>
+    <h3>More info on our candidates</h3>
     <?php echo randImages2($candidates); ?>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque et tortor id ipsum tincidunt dignissim id ut nibh. Morbi quis augue pretium, auctor sem eget, egestas eros. Mauris justo mauris, accumsan egestas enim in, ullamcorper vestibulum neque. Curabitur porttitor ante turpis, id venenatis leo tristique et.</p>
 </aside>
