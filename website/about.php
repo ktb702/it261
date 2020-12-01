1de386f9b45801cd07e3ea76a70d41baa9134e17
ktb702 <59974874+ktb702@users.noreply.github.com>
2020-11-30 23:19:15 -0700

    Added about.php

---
+++ website/about.php
@@ -0,0 +1,13 @@
+<?php
+include('includes/config.php');
+include('includes/header.php');
+?>
+    <div id="wrapper">
+
+    <h1> <?php echo $mainHeadline;  ?> </h1>
+    <a href="images/books.jpg"><img src="images/books.jpg" alt="Books Table" class="img-table"></a>
+
+     
+<?php 
+include('includes/footer.php');
+?>
